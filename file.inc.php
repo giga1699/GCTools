96885ea06476fe4c91d00fe5571c3666e503d368
Giga Murphy <giga1699@gmail.com>
2011-04-28 19:27:25 -0400

    Created file class

---
+++ file.inc.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Filename: file.inc.php
+ * @author J. "Giga" Murphy <giga1699@gmail.com>
+ * Purpose: Provide file functionality.
+ * @version 0.0.1
+ * File created: 28APR2011
+ * File modified: 28APR2011
+ * @package GCTools
+ * @subpackage File
+ * 
+ * Changelog:
+ */
+
+//Declare the namespace
+//namespace GCTools/File;
+
+class File {
+	
+}
+
+?>
