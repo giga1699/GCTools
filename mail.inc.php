46c07043e4b0d11b57219ba7f2cd96a3595fcd0b
Giga Murphy <giga1699@gmail.com>
2011-04-28 19:39:02 -0400

    Moved most of the attachment stuff to the File class, and implemented File into Attachment

--- mail.inc.php
+++ mail.inc.php
@@ -20,18 +20,23 @@
 //Declare the namespace
 //namespace GCTools/Mail;
 
+//Requires File class
+require_once("file.inc.php");
+
 //Attachment class
-class Attachment {
-	protected $fileName; //File name
-	protected $fileMIMEType; //File MIME type
-	protected $fileBuffer; //Holds contents of the file
-	protected $fileSize; //Holds the size of the file
-	protected $noError; //TRUE if there wasn't an error adding the file
+class Attachment extends File {
+	protected $theFile;
+	protected $noError;
 	
 	//Constructor
 	public function Attachment($file) {
 		//Add the file
-		$this->noError = $this->addFile($file);
+		$this->theFile = new File($file);
+		
+		if ($this->theFile->hadError())
+			$this->noError = FALSE;
+		else
+			$this->noError = TRUE;
 	}
 	
 	/*
@@ -53,142 +58,6 @@ class Attachment {
 			return FALSE;
 		else
 			return TRUE;
-	}
-	
-	/*
-	 * getFileName() function
-	 * 
-	 * No inputs
-	 * 
-	 * This function returns the filename of the file to be attached.
-	 * 
-	 * Returns the file name, or FALSE if it was not set.
-	 */
-	public function getFileName() {
-		/* Precondition: A file has been loaded. */
-		/* Postcondition: Return the value of fileName */
-		
-		if(isset($this->fileName))
-			return $this->fileName;
-		else
-			return FALSE;
-	}
-	
-	/*
-	 * getMIMEType() function
-	 * 
-	 * No inputs
-	 * 
-	 * This function returns the MIME type of the file to be attached.
-	 * 
-	 * Returns the MIME type, or FALSE if it was not set.
-	 */
-	public function getMIMEType() {
-		/* Precondition: A file has been loaded. */
-		/* Postcondition: Return the value of fileMIMEType */
-		
-		if(isset($this->fileMIMEType))
-			return $this->fileMIMEType;
-		else
-			return FALSE;
-	}
-	
-	/*
-	 * getSize() function
-	 * 
-	 * No inputs
-	 * 
-	 * This function returns the size of the file to be attached.
-	 * 
-	 * Returns file size, or FALSE if it was not set.
-	 */
-	public function getSize() {
-		/* Precondition: A file has been loaded. */
-		/* Postcondition: The file size is returned. */
-		
-		if(isset($this->fileSize))
-			return $this->fileSize;
-		else
-			return FALSE;
-	}
-	
-	/*
-	 * getFile() function
-	 * 
-	 * No inputs
-	 * 
-	 * This function provides the user access to get the buffered string of the file.
-	 * 
-	 * Returns the buffered file string, or FALSE if it's not set.
-	 */
-	public function getFile() {
-		/* Precondition: A file has been loaded. */
-		/* Postcondition: The file in the buffer is returned. */
-		
-		if(isset($this->fileBuffer))
-			return $this->fileBuffer;
-		else
-			return FALSE;
-	}
-	
-	/*
-	 * addFile($file) function
-	 * 
-	 * $file => Defines the location of the file to be attached
-	 * 
-	 * This function does the loading of the attachment. It sets most of the vaiables
-	 * for the entire class.
-	 * 
-	 * Returns TRUE on success, and FALSE on failure.
-	 */
-	private function addFile($file) {
-		/* Precondition: A file name, with location, is given */
-		/* Postcondition: The file is made into an attachment.
-		 * Returns FALSE if anything fails.
-		 */
-		
-		//Ensure we have a file, not a directory
-		if (!is_file($file))
-			return FALSE;
-			
-		//Get file name
-		$this->fileName = basename($file);
-		if (empty($this->fileName))
-			return FALSE;
-		
-		//Get file size
-		$this->fileSize = filesize($file);
-		if ($this->fileSize == 0 || $this->fileSize == FALSE)
-			return FALSE;
-		
-		//Get MIME Type
-		//Check if we have the right version to use Fileinfo
-		if (defined('PHP_MAJOR_VERSION') && PHP_MAJOR_VERSION >= 5 && PHP_MINOR_VERSION >= 3) {
-			//We have the right version to use Fileinfo
-			
-			//Create new instance of finfo
-			$finfo = finfo_open(FILEINFO_MIME_TYPE);
-			
-			//Add MIME type
-			$this->fileMIMEType = finfo_file($finfo, $file);
-			if ($this->fileMIMEType == FALSE)
-				return FALSE;
-			
-			//Close finfo class
-			finfo_close($finfo);
-		}
-		else {
-			//We don't have the right version, must use a different method
-			$this->fileMIMEType = mime_content_type($file);
-		}
-		
-		//Read file into the buffer
-		$this->fileBuffer = chunk_split(base64_encode(file_get_contents($file)));
-		if (!$this->fileBuffer)
-			return FALSE;
-		
-		//We're done, so return TRUE
-		return TRUE;
 	}
 }
 
