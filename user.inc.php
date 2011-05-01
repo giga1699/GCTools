2e51b4f0e929f84a01157585075b4b361e267e43
Giga Murphy <giga1699@gmail.com>
2011-05-01 15:20:10 -0400

    Created user.inc.php

---
+++ user.inc.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Filename: user.inc.php
+ * @author J. "Giga" Murphy <giga1699@gmail.com>
+ * Purpose: Provide user functionality.
+ * @version 0.0.1
+ * File created: 01MAY2011
+ * File modified: 01MAY2011
+ * @package GCTools
+ * @subpackage User
+ * 
+ * Changelog:
+ */
+
+?>
