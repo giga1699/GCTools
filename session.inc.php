d13ad2146d9a15160856a5d0f1139fa4dd35d9e1
Giga Murphy <giga1699@gmail.com>
2011-05-02 23:16:13 -0400

    Added init function to Session class. Session class is using the Singleton pattern

--- session.inc.php
+++ session.inc.php
@@ -13,4 +13,40 @@
 //Define namespace
 //namespace GCTools/Session;
 
+//Class uses Singleton pattern because you can have only 1 session open at a time.
+class Session {
+	private static $instance; //Holds the instance of the session class
+	
+	private function Session() {
+		//Prevent user from creating the class
+	}
+	
+	/*
+	 * init() function
+	 * 
+	 * No inputs
+	 * 
+	 * This function checks if a session has been created already. If one
+	 * has not been created, then the init function will create one. Otherwise
+	 * it will return the current instance.
+	 * 
+	 * Returns the current instance of the Session, or FALSE
+	 */
+	public function init() {
+		//Precondition: None
+		//Postcondition: Return the current instance of the Session, or FALSE
+		
+		if (!isset(self::$instance)) {
+			if (session_start()) {
+				$c = __CLASS__;
+				self::$instance = new $c;
+			}
+			else
+				throw new Exception("Unable to start session");
+		}
+		
+		return self::$instance;
+	}
+}
+
 ?>
