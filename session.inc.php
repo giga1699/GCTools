fddf405a527723cd7dfb59f26523a18fdeb6532c
Giga Murphy <giga1699@gmail.com>
2011-05-02 23:07:55 -0400

    Added session.inc.php file

---
+++ session.inc.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Filename: session.inc.php
+ * @author J. "Giga" Murphy <giga1699@gmail.com>
+ * Purpose: Provide session functionality.
+ * @version 0.0.1
+ * File created: 02MAY2011
+ * File modified: 02MAY2011
+ * @package GCTools
+ * @subpackage Session
+ */
+
+//Define namespace
+//namespace GCTools/Session;
+
+?>
