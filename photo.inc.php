c18f44a609e9dca50586ed26cd7d924b3ad4fa82
Giga Murphy <giga1699@gmail.com>
2011-05-03 17:40:02 -0400

    Changed how images are resized to prevent distortion of image.

--- photo.inc.php
+++ photo.inc.php
@@ -181,11 +181,25 @@ class Picture {
 		if (!isset($this->picWidth) || !isset($this->picHeight) || !isset($width) || !isset($height))
 			return FALSE;
 		
+		//Get scaling
+		$xscale = $width/$this->picWidth;
+		$yscale = $height/$this->picHeight;
+		
+		//Calculate new width and height
+		if ($xscale > $yscale) {
+			$newwidth = round($width/(1/$xscale));
+			$newheight = round($height/(1/$xscale));
+		}
+		else {
+			$newwidth = round($width/(1/$yscale));
+			$newheight = round($height/(1/$yscale));
+		}
+		
 		//Create new image
-		$new = imagecreatetruecolor($width, $height);
+		$new = imagecreatetruecolor($newwidth, $newheight);
 		
 		//Resize the image
-		$resize = imagecopyresampled($new, $this->picGDRes, 0,0,0,0, $width, $height, $this->picWidth, $this->picHeight);
+		$resize = imagecopyresampled($new, $this->picGDRes, 0,0,0,0, $newwidth, $newheight, $this->picWidth, $this->picHeight);
 		if (!$resize) {
 			imagedestroy($new);
 			return FALSE;
