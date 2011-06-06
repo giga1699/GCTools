7c5bf4fa0f935407f72fbf198715c69465aa25b5
Jimmy Murphy <jmurphy@ffg.com>
2011-06-06 12:06:17 -0400

    Created ldap.inc.php file

---
+++ ldap.inc.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Filename: ldap.inc.php
+ * @author: J. "Giga" Murphy <giga1699@gmail.com>
+ * Purpose: Provide LDAP support
+ * @version: 0.0.1
+ * File created: 06JUN2011
+ * @package GCTools
+ * @subpackage LDAP
+ */
+
+//namespace GCTools/LDAP;
+?>
