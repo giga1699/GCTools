7bef869edb4990fa5ad1efde2328bffacad55932
Jimmy Murphy <jmurphy@ffg.com>
2011-06-06 12:27:48 -0400

    Updated ldap.inc.php some more

--- ldap.inc.php
+++ ldap.inc.php
@@ -10,4 +10,52 @@
  */
 
 //namespace GCTools/LDAP;
+
+//LDAP Class
+class LDAP {
+	protected $ldapServer;
+	protected $ldapPort;
+	protected $ldapLink;
+	private $ldapUser;
+	private $ldapPassword;
+	protected $baseDN;
+	protected $lastError;
+	
+	public function LDAP($host, $errorCallback=NULL, $port=NULL) {
+		//Precondition: At least $host should be defined
+		//Postcondition: Create a connection to the LDAP server
+		
+		//Check if the LDAP libary is loaded
+		if (!extension_loaded('ldap')) {
+			//Extension not loaded, so load based on OS
+			if (strtoupper(substr(PHP_OS, 0, 3)) === 'WIN') {
+				dl('php_ldap.dll') or die("Unable to load libraries. Please contact your IT support staff.");
+			}
+			else {
+				dl('ldap.so') or die("Unable to load libraries. Please contact your IT support staff.");
+			}
+		}
+		
+		if (!isset($host))
+			throw new Exception("A LDAP server must be defined");
+		
+		$this->ldapServer = $host;
+		
+		if (isset($port))
+			$this->ldapPort = $port;
+		
+		//Connect
+		$this->connect();
+	}
+	
+	private function connect() {
+		//Precondition: None
+		//Postcondition: Attempt to make a connection to the LDAP server
+		
+		$this->ldapLink = ldap_connect($this->ldapServer, (isset($this->ldapPort) ? $this->ldapPort : "389"));
+		
+		if (!$this->ldapLink)
+			$this->throwError();
+	}
+}
 ?>
