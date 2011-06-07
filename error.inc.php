9669e61bf60d6e9f27009cf28618432afc66d1ee
Jimmy Murphy <jmurphy@ffg.com>
2011-06-07 14:57:53 -0400

    Added PHP error handler as GCErrorHandler.

--- error.inc.php
+++ error.inc.php
@@ -17,7 +17,7 @@ class Error {
 	protected $errorTo; //E-mail to send error messages
 	protected $errorSubject; //E-Mail subject when sending errors
 	
-	public function Error($to, $from) {
+	public function Error($to, $from, $subject=NULL) {
 		//Precondition: Both from and to should be set
 		//Postcondition: Set errorFrom and errorTo
 		
@@ -26,9 +26,15 @@ class Error {
 		
 		$this->errorFrom = $from;
 		$this->errorTo = $to;
+		
+		if (isset($subject))
+			$this->errorSubject = $subject;
 	}
 	
 	public function setErrorSubject($errorSubject) {
+		//Preconditoin: $errorSubject should be defined
+		//Postcondition: Set the errorSubject
+		
 		if (!isset($errorSubject))
 			return FALSE;
 		
@@ -46,6 +52,64 @@ class Error {
 		
 		echo "Sending message...\nTo: " . $this->errorTo . "\nFrom: " . $this->errorFrom . "\nSubject: " . $this->errorSubject . "\nMessage: " . $errorMessage . "\n\n\n";
 		mail($this->errorTo, (isset($this->errorSubject) ? $this->errorSubject : "GCTools Error Message"), $errorMessage, "From: " . $this->errorFrom);
+	}
+}
+
+class GCErrorHandler extends Error {
+	public function ErrorHandler($to, $from, $subject=NULL) {
+		//Precondition: Both from and to should be set
+		//Postcondition: Set-up Error class
+		
+		try {
+			$this->Error($to, $from, $subject);
+		}
+		catch (Exception $e) {
+			throw new Exception($e);
+		}
+	}
+	
+	public function setGCErrorGlobally($errors=NULL) {
+		//Precondition: None
+		//Postcondition: Set the PHP error handler to GCErrorHandler->handleError
+		
+		set_error_handler(array($this, "handleError"), (isset($errors) ? $errors : NULL));
+	}
+	
+	public function handleError($errno, $errstr, $errfile=NULL, $errline=NULL, $errcontext=NULL) {
+		//Precondition: $errno and $errstr should be defined
+		//Postcondition: Send an e-mail with the information about the error
+		
+		$errorMessage = "A PHP error has occured with the following conditions:\n\n";
+		
+		//Handle errorno
+		switch($errorno) {
+			case E_USER_ERROR:
+				$errorMessage .= "FATAL ERROR ";
+			break;
+			case E_USER_WARNING:
+				$errorMessage .= "WARNING ERROR ";
+			break;
+			case E_USER_NOTICE:
+				$errorMessage .= "NOTICE ERROR ";
+			break;
+			default:
+				$errorMessage .= "UNKNOWN ERROR ";
+			break;
+		}
+		
+		$errorMessage .= "[" . $errno . "]: " . $errstr . "\n\n";
+		
+		if (isset($errfile)) {
+			$errorMessage .= "Filename: " . $errfile . "\n";
+			
+			if (isset($errline))
+				$errorMessage .= "Line: " . $errline . "\n";
+			
+			if (isset($errcontext))
+				$errorMessage .= "\nError Context:\n" . $errcontext . "\n\n";
+		}
+		
+		$this->sendError($errorMessage);
 	}
 }
 
