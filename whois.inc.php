61f67e68aeec00d64a6607511fbf0943668b8d9f
giga1699 <giga1699@gmail.com>
2011-12-11 00:51:42 -0500

    Added some comments to Whois class

--- whois.inc.php
+++ whois.inc.php
@@ -31,6 +31,12 @@ class Whois {
 	}
 	
 	public function ValidDomain($domain) {
+		//Precondition: A domain should be given
+		/*
+		 * Postcondition: If a given Top Level Domain (TLD) is in
+		 * the server listing, then we can look up the domain
+		 * information.
+		 */
         $domain = explode(".", $domain);
         $tld = strtolower($domain[count($domain)-1]);
         
@@ -38,29 +44,49 @@ class Whois {
     }
     
     public function Lookup($domain) {
+    	//Precondition: A valid domain should be provided
+    	//Postcondition: Return the domain whois data, or FALSE otherwise
+    	
+    	if (!$this->ValidDomain($domain))
+    		return FALSE;
+    	
+    	//Get TLD
         $domain = explode(".", $domain);
         $tld = strtolower($domain[count($domain)-1]);
         
+        //Open up WHOIS server
         $link = fsockopen($this->servers[$tld], "43");
         
+        //If we couldn't connect, then return FALSE
         if (!$link)
             return FALSE;
         
+        //Write the lookup domain to the WHOIS server
         fwrite($link, implode(".", $domain) . "\r\n");
         
+        //Prepare return variable
         $data = "";
         
+        //Get all the WHOIS data
         while (!feof($link)) {
             $data .= fgets($link);
         }
         
+        //Close link to WHOIS server
         fclose($link);
         
+        //Return the data
         return $data;
     }
     
     public function getCreateDate($data) {
-    	//Create date
+    	//Precondition: Some kind of data should be given
+    	/*
+    	 * Postcondition: Attempt to find the creation date of the domain.
+    	 * Return the creation date on success, and FALSE otherwise.
+    	 */
+    	
+    	//Find create date
         if (preg_match("/Created on:[\s]{0,}[0-9]{1,2}-[A-Za-z]{1,3}-[0-9]{1,4}/i", $data, $matches)) {
             preg_match("/[0-9]{1,2}-[A-Za-z]{1,3}-[0-9]{2,4}/", $matches[0], $test);
             $created = $test[0];
@@ -77,14 +103,23 @@ class Whois {
             preg_match("/[0-9]{1,2}-[A-Za-z]{1,3}-[0-9]{2,4}\s[0-9]{1,2}:[0-9]{1,2}:[0-9]{1,2}\s[A-Z]{3}/", $matches[0], $test);
             $created = $test[0];
         }
-        else
-            $created = "ERR";
-           
+        else {
+        	//Couldn't find it.
+            $created = FALSE;
+        }
+        
+        //Return creation date
         return $created;
     }
     
     public function getExpireDate($data) {
-    	//Expire date
+    	//Precondition: Some kind of data should be given
+    	/*
+    	 * Postcondition: Attempt to find the expiration date of the domain.
+    	 * Return the expiration date on success, and FALSE otherwise.
+    	 */
+    	
+    	//Find expire date
         if (preg_match("/Expires on:[\s]{0,}[0-9]{1,2}-[A-Za-z]{1,3}-[0-9]{1,4}/i", $data, $matches)) {
             preg_match("/[0-9]{1,2}-[A-Za-z]{1,3}-[0-9]{2,4}/", $matches[0], $test);
             $expires = $test[0];
@@ -102,7 +137,7 @@ class Whois {
             $expires = $test[0];
         }
         else
-            $expires = "ERR";
+            $expires = FALSE;
         
         return $expires;
     }
