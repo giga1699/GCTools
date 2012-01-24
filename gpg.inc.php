eaee7fe10cfc66f9faf994167eaec2ee806acb7f
Jimmy Murphy <giga1699@gmail.com>
2012-01-24 01:16:00 -0500

    Minor updates

--- gpg.inc.php
+++ gpg.inc.php
@@ -15,4 +15,26 @@
  * TODO: Implement GPG functions
  * Good reference http://devzone.zend.com/1278/using-gnupg-with-php/
  */
+
+//Error codes
+
+class GCGPG {
+	/*
+	 * $gpgModule => Defines if the PHP GPG module is being used, or not
+	 */
+	protected $gpgModule;
+
+	public function GCGPG() {
+		//Check if the GPG module is loaded, and try to load it.
+
+		//If all else fails, set module to false.
+		$this->gpgModule = FALSE;
+	}
+
+	//Encrypt
+	//Decrypt
+	//Sign
+	//Validate
+	//Pull key
+}
 ?>
