f8e198d99b0e29270227b7008ef1a25cd35ce002
Jimmy Murphy <giga1699@gmail.com>
2012-01-24 01:29:49 -0500

    Finished up init, and layed out some new functions for the class.

--- gpg.inc.php
+++ gpg.inc.php
@@ -7,6 +7,8 @@
  * File created: 24JAN2012
  * @package GCTools
  * @subpackage GPG
+ *
+ * Reference PECL Package: http://pecl.php.net/package/gnupg
  */
 
 //namespace GCTools/GPG;
@@ -21,14 +23,38 @@
 class GCGPG {
 	/*
 	 * $gpgModule => Defines if the PHP GPG module is being used, or not
+	 *
+	 * $gpgRes => Variable to hold the GnuPG module resource
 	 */
 	protected $gpgModule;
+	private $gpgRes;
 
 	public function GCGPG() {
 		//Check if the GPG module is loaded, and try to load it.
+		if (!extension_loaded('gnupg')) {
+			//Extension not loaded, so load based on OS
+			if (strtoupper(substr(PHP_OS, 0, 3)) === 'WIN') {
+				//Load for windows
+				throw new Exception("This feature has not been added for your operating system yet.");
+			}
+			else {
+				if (!dl('gnupg.so'))
+					$this->gpgModule = FALSE;
+				else
+					$this->gpgModule = TRUE;
+			}
+		}
+		else
+			$this->gpgModule = TRUE;
 
-		//If all else fails, set module to false.
-		$this->gpgModule = FALSE;
+		if ($this->gpgModule) {
+			//Init the GnuPG module resource
+			$this->gpgRes = new gnupg();
+		}
+		else {
+			//Unknown at this time. Will have to work on implementing the command line version of GPG.
+			throw new Exception("This feature is not yet implemented.");
+		}
 	}
 
 	//Encrypt
@@ -36,5 +62,6 @@ class GCGPG {
 	//Sign
 	//Validate
 	//Pull key
+	//Import key
 }
 ?>
