67c4c03540baf2ce38129740bc785a04999bba98
Jimmy Murphy <giga1699@gmail.com>
2012-01-24 01:05:48 -0500

    Added gpg file

---
+++ gpg.inc.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Filename: gpg.inc.php
+ * @author: J. "Giga" Murphy <giga1699@gmail.com>
+ * Purpose: Provide gpg support
+ * @version: 0.0.1
+ * File created: 24JAN2012
+ * @package GCTools
+ * @subpackage GPG
+ */
+
+//namespace GCTools/GPG;
+
+/*
+ * TODO: Implement GPG functions
+ * Good reference http://devzone.zend.com/1278/using-gnupg-with-php/
+ */
+?>
